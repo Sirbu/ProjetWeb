45c6278da243cca2a8771d53095fb1df8b4bb423
Sirbulescu Alexis <alexis.sirbulescu@openmailbox.org>
2016-05-12 15:20:45 +0200

    Finnition du script de création de la base de donnée. Création du script d'insertion de donnée, avec un petit jeu de données de test.

---
+++ Documents/database-data.sql
@@ -0,0 +1,23 @@
+-- Laboratoires
+INSERT INTO Laboratoire VALUES (1, 'IRIT', '118 route narbonne Toulouse', 'Informatique Toulouse', 'Informatique');
+
+-- Équipes
+INSERT INTO Equipe VALUES (1, 'Service Integration and Network Administration', 'SIERA', 'Réseaux', 'Our works aim at controlling not only last generation communication infrastructures and services, but also dynamically aggregated, distributed and trans-organizational complex applications and systems. Thus, our researches define and evaluate new management paradigms and bring architectures, platforms, tools or normalization contributions to the community. The target application domains are networks and services, together with e-learning.', 1);
+
+-- Chercheurs
+INSERT INTO Chercheur VALUES (1, 'aoun', 'papaaoun', 1, 'Aoun', 'André', 'aoun@irit.fr', '0534662395', '1');
+
+-- Publications
+INSERT INTO Publication VALUES (1, 'Théorie des réseaux !', '12/05/2016', 'ftp://serveur.fr');
+
+-- Calendrier
+INSERT INTO Calendrier VALUES (1, '11/05/2016', '30/05/2016');
+
+-- Projet
+INSERT INTO Projet VALUES (1, 'SKYNET', '500000', 'Plateforme espionnage téléphonique', 1, 1);
+
+-- Participe
+INSERT INTO Participe VALUES (1, 1, 'Coordinateur');
+
+-- Publie
+INSERT INTO Publie VALUES (1, 1);
--- Documents/database-structure.sql
+++ Documents/database-structure.sql
@@ -1,14 +1,15 @@
 ﻿DROP TYPE IF EXISTS STATUT CASCADE;
 DROP TYPE IF EXISTS DOC CASCADE;
 DROP TYPE IF EXISTS DOM CASCADE;
-DROP TYPE IF EXISTS TACHE CASCADE;
+DROP TYPE IF EXISTS TYPE_TACHE CASCADE;
 CREATE TYPE STATUT as ENUM ('Responsable', 'Coordinateur', 'Chercheur');
 CREATE TYPE DOM as ENUM ('Biologie', 'Informatique', 'Chimie', 'Physique');
 CREATE TYPE DOC as ENUM ('Compte rendu réunion', 'Rapport expérience', 'Brouillon', 'Livrable');
-CREATE TYPE TACHE as ENUM('Réunion', 'Expérience', 'Tests', 'Documentation', 'Jalon', 'Avancement');
+CREATE TYPE TYPE_TACHE as ENUM('Réunion', 'Expérience', 'Tests', 'Documentation', 'Jalon', 'Avancement');
 
 DROP TABLE IF EXISTS Chercheur CASCADE;
-CREATE TABLE Chercheur (idCh SERIAL NOT NULL,
+CREATE TABLE Chercheur (
+idCh SERIAL NOT NULL,
 loginCh VARCHAR(10),
 passCh VARCHAR(25),
 numBureau INT,
@@ -19,20 +20,21 @@ numTel INT,
 idEquipe INT,
 PRIMARY KEY (idCh) );
 
-DROP TABLE IF EXISTS Équipe CASCADE;
-CREATE TABLE Équipe (idEquipe SERIAL NOT NULL,
-nomEq VARCHAR(25),
+-- penser à supprimer la table Equipe avec accent
+DROP TABLE IF EXISTS Equipe CASCADE;
+CREATE TABLE Equipe (idEquipe SERIAL NOT NULL,
+nomEq VARCHAR(50),
 sigle VARCHAR(10),
-specialite VARCHAR(25),
-description TEXT,
+specialite VARCHAR(50),
+description VARCHAR(500),
 idLabo INT NOT NULL,
 PRIMARY KEY (idEquipe) )  ;
 
 DROP TABLE IF EXISTS Laboratoire CASCADE;
 CREATE TABLE Laboratoire (idLabo SERIAL NOT NULL,
 nomLabo VARCHAR(25),
-adresseLabo VARCHAR(25),
-descriptionLabo TEXT,
+adresseLabo VARCHAR(50),
+descriptionLabo VARCHAR(500),
 Domaine DOM,
 PRIMARY KEY (idLabo) )  ;
 
@@ -40,23 +42,23 @@ DROP TABLE IF EXISTS Publication CASCADE;
 CREATE TABLE Publication (idPubli SERIAL NOT NULL,
 titre VARCHAR(50),
 datePubli DATE,
-urlPub INT,
+urlPub VARCHAR(100),
 PRIMARY KEY (idPubli) )  ;
 
 DROP TABLE IF EXISTS Message CASCADE;
 CREATE TABLE Message (idDiscussion SERIAL NOT NULL,
 titreMessage VARCHAR(50),
 objet VARCHAR(100),
-contenuMess TEXT,
+contenuMess VARCHAR(500),
 dateEnvoi INT,
 idCh INT NOT NULL,
 idProjet INT NOT NULL,
 PRIMARY KEY (idDiscussion) )  ;
 
-DROP TABLE IF EXISTS Tâche CASCADE;
-CREATE TABLE Tâche (idTache SERIAL NOT NULL,
+DROP TABLE IF EXISTS Tache CASCADE;
+CREATE TABLE Tache (idTache SERIAL NOT NULL,
 nomTache VARCHAR(50),
-typeTache TACHE,
+typeTache TYPE_TACHE,
 descriptionTaches VARCHAR(500),
 idProjet INT NOT NULL,
 idCal INT NOT NULL,
@@ -66,7 +68,7 @@ DROP TABLE IF EXISTS Projet CASCADE;
 CREATE TABLE Projet (idProjet SERIAL NOT NULL,
 nomProjet VARCHAR(25),
 budget INT,
-DescriptionProjet TEXT,
+DescriptionProjet VARCHAR(500),
 idCh INT NOT NULL,
 idCal INT NOT NULL,
 PRIMARY KEY (idProjet) )  ;
@@ -101,8 +103,8 @@ idPubli INT NOT NULL,
 PRIMARY KEY (idCh,
  idPubli) )  ;
 
-DROP TABLE IF EXISTS Dépose CASCADE;
-CREATE TABLE Dépose (idDoc SERIAL NOT NULL,
+DROP TABLE IF EXISTS Depose CASCADE;
+CREATE TABLE Depose (idDoc SERIAL NOT NULL,
 idCh INT NOT NULL,
 PRIMARY KEY (idDoc,
  idCh) )  ;
@@ -113,20 +115,20 @@ idTache INT NOT NULL,
 PRIMARY KEY (idCh,
  idTache) )  ;
 
-ALTER TABLE Chercheur ADD CONSTRAINT FK_Chercheur_idEquipe FOREIGN KEY (idEquipe) REFERENCES Équipe (idEquipe);
+ALTER TABLE Chercheur ADD CONSTRAINT FK_Chercheur_idEquipe FOREIGN KEY (idEquipe) REFERENCES Equipe (idEquipe);
 
-ALTER TABLE Équipe ADD CONSTRAINT FK_Équipe_idLabo FOREIGN KEY (idLabo) REFERENCES Laboratoire (idLabo);
+ALTER TABLE Equipe ADD CONSTRAINT FK_Equipe_idLabo FOREIGN KEY (idLabo) REFERENCES Laboratoire (idLabo);
 ALTER TABLE Message ADD CONSTRAINT FK_Message_idCh FOREIGN KEY (idCh) REFERENCES Chercheur (idCh);
 ALTER TABLE Message ADD CONSTRAINT FK_Message_idProjet FOREIGN KEY (idProjet) REFERENCES Projet (idProjet);
-ALTER TABLE Tâche ADD CONSTRAINT FK_Tâche_idProjet FOREIGN KEY (idProjet) REFERENCES Projet (idProjet);
-ALTER TABLE Tâche ADD CONSTRAINT FK_Tâche_idCal FOREIGN KEY (idCal) REFERENCES Calendrier (idCal);
+ALTER TABLE Tache ADD CONSTRAINT FK_Tache_idProjet FOREIGN KEY (idProjet) REFERENCES Projet (idProjet);
+ALTER TABLE Tache ADD CONSTRAINT FK_Tache_idCal FOREIGN KEY (idCal) REFERENCES Calendrier (idCal);
 ALTER TABLE Projet ADD CONSTRAINT FK_Projet_idCh FOREIGN KEY (idCh) REFERENCES Chercheur (idCh);
 ALTER TABLE Projet ADD CONSTRAINT FK_Projet_idCal FOREIGN KEY (idCal) REFERENCES Calendrier (idCal);
 ALTER TABLE Publie ADD CONSTRAINT FK_Publie_idCh FOREIGN KEY (idCh) REFERENCES Chercheur (idCh);
 ALTER TABLE Publie ADD CONSTRAINT FK_Publie_idPubli FOREIGN KEY (idPubli) REFERENCES Publication (idPubli);
-ALTER TABLE Dépose ADD CONSTRAINT FK_Dépose_idDoc FOREIGN KEY (idDoc) REFERENCES Document (idDoc);
-ALTER TABLE Dépose ADD CONSTRAINT FK_Dépose_idCh FOREIGN KEY (idCh) REFERENCES Chercheur (idCh);
+ALTER TABLE Depose ADD CONSTRAINT FK_Depose_idDoc FOREIGN KEY (idDoc) REFERENCES Document (idDoc);
+ALTER TABLE Depose ADD CONSTRAINT FK_Depose_idCh FOREIGN KEY (idCh) REFERENCES Chercheur (idCh);
 ALTER TABLE effectue ADD CONSTRAINT FK_effectue_idCh FOREIGN KEY (idCh) REFERENCES Chercheur (idCh);
-ALTER TABLE effectue ADD CONSTRAINT FK_effectue_idTache FOREIGN KEY (idTache) REFERENCES Tâche (idTache);
+ALTER TABLE effectue ADD CONSTRAINT FK_effectue_idTache FOREIGN KEY (idTache) REFERENCES Tache (idTache);
 ALTER TABLE Participe ADD CONSTRAINT FK_Participe_idCh FOREIGN KEY (idCh) REFERENCES Chercheur (idCh);
 ALTER TABLE Participe ADD CONSTRAINT FK_Participe_idProjet FOREIGN KEY (idProjet) REFERENCES Projet (idProjet);
