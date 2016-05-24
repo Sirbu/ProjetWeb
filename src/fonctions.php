5034168ecb44e22045b797a4247200d7b4bae75a
BUGEP <buge.pascal@gmail.com>
2016-05-24 15:40:03 +0200

    Messages connectés avec JO
    Ajout d'une fonction
    ajout du placement d'un bouton css
    ATTENTION le password dans fonctions.php est post et non stri

--- src/fonctions.php
+++ src/fonctions.php
@@ -1,7 +1,7 @@
 <?php 
 	function connectDB()
 	{
-		$dbconnect = pg_connect("host=localhost port=5432 dbname=RCP user=postgres password=post");
+		$dbconnect = pg_connect("host=localhost port=5432 dbname=RCP user=postgres password=stri");
 	    if(!$dbconnect)
 	    {
 	        header('Location: erreur.php?error=bdd_connect');
@@ -11,4 +11,20 @@
 	    	return $dbconnect;
 	    }
 	}
+
+
+	function idLastElement($id,$table)
+	{
+		$requete = "SELECT max($id) FROM $table;";
+        $resultat = pg_query(connectDB(), $requete);
+        $lignes = pg_fetch_row($resultat);
+        if (!isset($lignes[0])){
+        	$cpt = 1;
+        }
+        else{
+        	$cpt = $lignes[0] + 1;
+        }
+        return $cpt;
+	}
+
  ?>
