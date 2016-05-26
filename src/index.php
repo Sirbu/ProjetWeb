7b028c0185bc590386380e2778e32f9de68feb46
Sirbulescu Alexis <alexis.sirbulescu@openmailbox.org>
2016-05-26 17:28:59 +0200

    Carousel terminé !

--- src/index.php
+++ src/index.php
@@ -186,44 +186,78 @@
                         </caption>
                     </div>
 
-                    <?php 
-                        // ici on prépare l'affichage du carousel
-                        $query = "SELECT "
-                     ?>
-
+                    
                     <div class="last-publi">
                         <caption><h2>Dernières publications :</h2></caption>
                         <br>
                         <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                              <ol class="carousel-indicators">
-                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                                <li data-target="#myCarousel" data-slide-to="1"></li>
-                                <li data-target="#myCarousel" data-slide-to="2"></li>
-                              </ol>
-                              <div class="carousel-inner">
-                                <div class="item active">
-                                    <img class="img-responsive center-block" src="Images/blue-paper-texture.jpg" alt="Slide 1" />
-                                    <div class="carousel-caption">
-                                        <h1>TOTO</h1>
-                                        <p>Description</p>
-                                    </div>
-                                </div>
-                                <div class="item">
-                                    <img class="img-responsive center-block" src="Images/blue-paper-texture.jpg" alt="Slide 2" />
-                                    <div class="carousel-caption">
-                                        <h1>TITI</h1>
-                                        <p>Description</p>
-                                    </div>
-                                </div>
-                                <div class="item">
-                                    <img class="img-responsive center-block" src="Images/blue-paper-texture.jpg" alt="Slide 3" />
-                                    <div class="carousel-caption">
-                                        <h1>TUTU<br>TITI</h1>
-                                        <p>Description</p>
-                                    </div>
-                                </div>
-                              </div>
+                              <ol class="carousel-indicators">    
+                    <?php 
+                        // ici on prépare l'affichage du carousel
+                        // c'est un beau bordel d'ailleurs parce qu'un
+                        // carousel ça prend un paquet de lignes et faire
+                        // tout ça avec des echo ça fait mal aux yeux...
+                        $query = "SELECT P.idpubli, titre, nomch FROM Publication as P, Publie as Pb, Chercheur as C"
+                                ." WHERE P.idpubli = Pb.idpubli"
+                                ." AND Pb.idch = C.idch"
+                                ." ORDER BY datePubli;";
 
+                        $result = pg_query($dbconnect, $query);
+                        if(!$result)
+                        {
+                            header('Location: erreur.php?error=err_SQL');
+                            die();
+                        }
+
+                        // nombre de publications à afficher
+                        // dans le carousel
+                        $nbr_publi = 3;
+
+                        $info_publi = pg_fetch_all($result);
+
+                        // on ne doit pas faire plus de slides que de publications
+                        // disponibles. Même si au départ on veut en afficher 3, il 
+                        // se peut qu'il n'y en aie que 2 dans la base de donnée.
+                        for($i = 0; $i < min($nbr_publi, count($info_publi)); $i++)
+                        {
+                            echo "\t\t\t\t\t<li data-target=\"#myCarousel\" data-slide-to=\"$i\" ";
+                            if($i == 0)
+                            {
+                                echo "class=\"active\"";
+                            }
+                            echo "></li>\n\t\t\t\t\t";
+                        }
+
+                        echo "</ol>
+                              <div class=\"carousel-inner\">
+                              ";
+
+                        for($i = 0; $i < min($nbr_publi, count($info_publi)); $i++)
+                        {
+                            echo "\n<div class=\"item ";
+                            if($i == 0)
+                            {
+                                echo "active";
+                            }
+                            echo "\">";
+                                
+                            echo  "<img class=\"img-responsive center-block\" src=\"Images/blue-paper-texture.jpg\""
+                                        . " alt=\"Slide $i\" />
+                                    <div class=\"carousel-caption\">";
+
+                            echo "          <a href=\"publication.php?idpublication=".$info_publi[$i]['idpubli']."\">
+                                                <h1>" . $info_publi[$i]['titre'] . "</h1>
+                                            </a>";
+                            echo "          <a href=\"chercheur.php?nomchercheur=".$info_publi[$i]['titre']."\">
+                                                <p>" . $info_publi[$i]['nomch'] . "</p>
+                                            </a>";
+
+                            echo"        </div>
+                                    </div>";
+                            
+                        }
+
+                     ?>
                               <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
                                 <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                                 <span class="sr-only">Previous</span>
